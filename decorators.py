e96ad9c07919f78685a7c1817b0d1f1a74596b3e
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2024-04-20 01:46:40 +0530

    removed comments

--- decorators.py
+++ decorators.py
@@ -6,36 +6,36 @@ import time
 
 # Example 1
 
-# def timer(func):
-#      def wrapper(*args, **kwargs):
-#           start = time.time()
-#           result = func(*args, **kwargs)
-#           end = time.time()
-#           total_time = round(end-start, 2)
-#           print(func.__name__, 'ran in', total_time, 'time')
-#           return result
-#      return wrapper
+def timer(func):
+     def wrapper(*args, **kwargs):
+          start = time.time()
+          result = func(*args, **kwargs)
+          end = time.time()
+          total_time = round(end-start, 2)
+          print(func.__name__, 'ran in', total_time, 'time')
+          return result
+     return wrapper
 
-# @timer  # Decorator - now this method will pass through timer function before running itself
-# def example_function(n):
-#      time.sleep(n)
+@timer  # Decorator - now this method will pass through timer function before running itself
+def example_function(n):
+     time.sleep(n)
 
-# example_function(2)
+example_function(2)
 
 # Example 2
 
-# def debug(func):
-#      def wrapper(*args, **kwargs):
-#           args_value = ', '.join(str(arg) for arg in args)
-#           kwargs_value = ', '.join(f"{k}:{v}" for k,v in kwargs.items())
-#           print(f"Calling: {func.__name__} with args {args_value} and {kwargs_value}")
-#           result = func(*args, **kwargs)
-#           return result
-#      return wrapper
+def debug(func):
+     def wrapper(*args, **kwargs):
+          args_value = ', '.join(str(arg) for arg in args)
+          kwargs_value = ', '.join(f"{k}:{v}" for k,v in kwargs.items())
+          print(f"Calling: {func.__name__} with args {args_value} and {kwargs_value}")
+          result = func(*args, **kwargs)
+          return result
+     return wrapper
 
-# @debug
-# def greet(name, greeting="Hello"):
-#      print(f"{greeting}, {name}")
+@debug
+def greet(name, greeting="Hello"):
+     print(f"{greeting}, {name}")
 
 
 # Example 3 - Caching
